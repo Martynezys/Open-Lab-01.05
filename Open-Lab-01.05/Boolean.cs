cac247480efffb7fa29bc4adf2ade77013861e9f
Martynezys <113421557+Martynezys@users.noreply.github.com>
2022-09-20 16:00:16 +0200

    kkk

--- Open-Lab-01.05/Boolean.cs
+++ Open-Lab-01.05/Boolean.cs
@@ -6,7 +6,8 @@ namespace Open_Lab_01._05
     {
         public bool Reverse(bool value)
         {
-            return Reverse(value);
+            return !value;
+            throw new NotImplementedException();
         }
     }
 }
